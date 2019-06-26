2933a0af989057811e1046ba91ca88a917af928b
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2019-06-26 13:58:27 +0200

    Merged viz function into main & add print wig option.

---
+++ CHANGELOG.md
@@ -0,0 +1,27 @@
+# ChangeLog
+
+## [0.5]
+- merged viz into the main rhocall framework
+- viz can now produce wig files
+
+## [0.4]
+New flag --v14 to accept new bcftools roh files.
+
+## [0.3]
+--version
+
+## [0.2]
+
+Single chromosomes. Still some edge cases left for non-standard operation.
+Classification pending. A test set has been added, but tests not yet written.
+Looking at current test set performance, advise the use of bcftools v1.2 rather
+than v.1.3 which seems to have broken something in their ROH functionality
+(see bcftools #529).
+
+## [0.1]
+
+Initial release! Passes auto-install and performs intended functions on a
+limited number of test samples.
+Many edge cases can remain. Classification of AZ variant types is only
+a placeholder right now. Future mode will include reading ped file,
+noting inheritance and sex.
--- rhocall/run_viz.py
+++ rhocall/run_viz.py
@@ -5,14 +5,13 @@ import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import math
 
-version = "42"
-
 #compute the ratio of homozygous snps across the genome
-def generate_bins(args):
+def generate_bins(input_vcf, window, filter, mnv, minqual, rsid, minaf, aftag,
+                  maxaf, minsnv):
     bins={}
     contigs={}
     #collect the number of hetrozygous and homozygous snps for each bin
-    for line in open(args.input_vcf):
+    for line in open(input_vcf):
         if line[0] == "#":
             if "##contig=<ID=" in line:
                 contig_id=line.split("##contig=<ID=")[-1].split(",")[0]
@@ -21,14 +20,14 @@ def generate_bins(args):
 
             if "#CHROM" in line:
                 for contig in contigs:
-                    bins[contig]=numpy.zeros((int(math.ceil(contigs[contig]/float(args.window))),2) )
+                    bins[contig]=numpy.zeros((int(math.ceil(contigs[contig]/float(window))),2) )
             continue
-        if not args.nofilter and not "\tPASS\t" in line:
+        if filter and not "\tPASS\t" in line:
             continue
 
         content=line.strip().split()
         #skip mnv calls
-        if not args.mnv:
+        if not mnv:
             if not len(content[3]) == 1 and not len(content[4]) == 1:
                 continue
 
@@ -36,7 +35,7 @@ def generate_bins(args):
         ok=True
         try:
             quality=float(content[5])
-            if quality < args.minqual:
+            if quality < minqual:
                 ok=False
         except:
             pass
@@ -44,15 +43,15 @@ def generate_bins(args):
             continue
 
         #rrsid filter(if enabled)
-        if args.rsid and not content[2].startswith("rs"):
+        if rsid and not content[2].startswith("rs"):
             continue
 
         #allele frequency filter
-        if args.minaf:
-            if ";{}=".format(args.aftag) in content[7]:
+        if minaf:
+            if ";{}=".format(aftag) in content[7]:
                 try:
-                    af=float(content[7].split(";{}=".format(args.aftag))[-1].split(";")[0])
-                    if af < args.minaf or af > args.maxaf:
+                    af=float(content[7].split(";{}=".format(aftag))[-1].split(";")[0])
+                    if af < minaf or af > maxaf:
                         continue
                 except:
                     print("Error parsing allele frequency.")
@@ -61,7 +60,7 @@ def generate_bins(args):
                 continue
 
 
-        pos=int(math.floor(int(content[1])/float(args.window)))
+        pos=int(math.floor(int(content[1])/float(window)))
         if "1/1" in content[-1] or "0/0" in content[-1] or "1|1" in content[-1]:
                 bins[content[0]][pos][1]+=1
         elif "./." in content[-1] or "./1" in content[-1]:
@@ -73,7 +72,7 @@ def generate_bins(args):
     for chromosome in bins:
         tmp_ratios=[]
         for window in bins[chromosome]:
-            if sum(window) < args.minsnv:
+            if sum(window) < minsnv:
                 tmp_ratios.append(-1)
             else:
                 tmp_ratios.append(window[1]/float(window[1]+window[0]))
@@ -81,10 +80,10 @@ def generate_bins(args):
     return  bins
 
 # take the rhocall output and find all reported ROH
-def extract_roh(args):
+def extract_roh(rho):
     roh={}
-    chrom_tot={}
-    for line in open(args.rho):
+
+    for line in open(rho):
         if line[0] == "#":
             continue
         elif "CHROMOSOME_TOT" in line:
@@ -95,20 +94,20 @@ def extract_roh(args):
             roh[content[0]]=[]
         roh[content[0]].append([int(content[1]),int(content[2])])
 
-    return(roh,chrom_tot)
+    return(roh)
 
 #create the plots, one for each chromosome, and print them to the assigned directory
-def generate_plots(binned_zygosity,roh,chrom_tot,args):
+def generate_plots(binned_zygosity,roh, window, pointsize, out_dir):
     for chromosome in binned_zygosity:
         if "GL" in chromosome:
             continue
 
-        posvector=numpy.array( range(0,len(binned_zygosity[chromosome])))*args.window/1000.0
+        posvector=numpy.array( range(0,len(binned_zygosity[chromosome])))*window/1000.0
 
         plt.figure(1)
         plt.subplot(111)
 
-        fraction=plt.scatter(posvector, binned_zygosity[chromosome], c="black",s=args.pointsize, alpha=0.5,marker = 'o',label='fraction')
+        fraction=plt.scatter(posvector, binned_zygosity[chromosome], c="black",s=pointsize, alpha=0.5,marker = 'o',label='fraction')
         median_fraction=numpy.median( binned_zygosity[chromosome][ numpy.where( binned_zygosity[chromosome] > -1  ) ])
         median, =plt.plot([0,max(posvector)],[median_fraction,median_fraction], linewidth=4, color='green',alpha=0.75,label="median fraction")
         ROH, =plt.plot([0,0],[-1,-1], linewidth=2, color='red',label="RHO")
@@ -129,33 +128,23 @@ def generate_plots(binned_zygosity,roh,chrom_tot,args):
         plt.ylabel('Fraction of homozygous snps')
         figure = plt.gcf()
         figure.set_size_inches(16, 10)
-        plt.savefig("{}/{}.png".format(args.dir,chromosome),dpi=100)
+        plt.savefig("{}/{}.png".format(out_dir,chromosome),dpi=100)
         plt.close()
 
+#create the plots, one for each chromosome, and print them to the assigned directory
+def generate_wig(binned_zygosity,roh, window, outfile):
 
-def main(args):
-    os.system( "mkdir {}".format(args.dir) )
-    binned_zygosity=generate_bins(args)
-    roh,chrom_tot=extract_roh(args)
-    generate_plots(binned_zygosity,roh,chrom_tot,args)
+    wigf = open(outfile, "w")
+    for chromosome in binned_zygosity:
+        if "GL" in chromosome:
+            continue
 
+        posvector=numpy.array( range(0,len(binned_zygosity[chromosome])))*window/1000.0
 
-## Argument parsing
-parser = argparse.ArgumentParser(description='Call runs of autozygosity.')
-parser.add_argument('--input_vcf','-i', type=str,required=True, help='Input (sorted) vcf file')
-parser.add_argument('--dir','-d', type=str,required=True, help='output directory, the files will be named dir/chr.png, one picture is printed per chromosome')
-parser.add_argument('--rho','-r', type=str,required=True, help='Input RHO file produced from rhocall')
-parser.add_argument('--window','-w', type=int,default=10000, help='window size(bases)')
-parser.add_argument('--minsnv','-m', type=int,default=2, help='minimum number of snvs for each plotted bin')
-parser.add_argument('--maxsnv','-M', type=int,default=20, help='maximum number of snvs for each plotted bin')
-parser.add_argument('--minaf',type=float,default=0.1, help='minimum allele frequency(this variable must be set to 0 if the allele frequency is not annotated)')
-parser.add_argument('--maxaf',type=float,default=0.9, help='maximum allele frequency')
-parser.add_argument('--mnv', action='store_true', help='include MNV')
-parser.add_argument('--aftag', type=str,default="1000GAF", help='the allele frequency tag')
-parser.add_argument('--minqual','-q', type=int,default=100, help='do not add snvs to a bin if there quality is less than this value')
-parser.add_argument('--pointsize','-p', type=int,default=8, help='Size of the points (pixels)')
-parser.add_argument('--rsid','-s', action='store_true', help='Skip variants not containing an rsid')
-parser.add_argument('--nofilter','-n', action='store_true', help='include variants, even if they are not labeled PASS')
-args = parser.parse_args()
-
-main(args)
+        fraction=plt.scatter(posvector, binned_zygosity[chromosome], c="black",s=pointsize, alpha=0.5,marker = 'o',label='fraction')
+        print('track type=wiggle_0 description="Fraction of homozygous snps"')
+        if chromosome in roh:
+            wigf.write("fixedStep chrom={} start=1, step={}", chromosome, window)
+            roh[chromosome]=numpy.array(roh[chromosome])/1000
+            for r in roh[chromosome]:
+                wigf.write(r)
